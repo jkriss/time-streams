ebc6a2f7d80e952feb4755b36160db8988a0da12
Jesse Kriss <jesse@jklabs.net>
2020-06-23 20:32:43 -0700

    Pulling weird hash auth thing from middleware

--- middleware/writer.js
+++ middleware/writer.js
@@ -2,12 +2,6 @@ const URL = require('url')
 const express = require('express')
 const bodyParser = require('body-parser')
 const { findStream } = require('./shared')
-const { verify } = require('./auth')
-
-function isAllowed(req, streamID) {
-  const secret = req.headers['authorization'] && req.headers['authorization'].split(' ')[1]
-  return verify({ secret, streamID })
-}
 
 function writerMiddleware(basePath) {
 
@@ -22,21 +16,16 @@ function writerMiddleware(basePath) {
     const url = URL.parse(req.url)
     const { stream, streamID } = await findStream(basePath, url, req)
     if (stream) {
-      const allowed = isAllowed(req, streamID)
-      if (!allowed) {
-        return res.status(401).send('Not authorized')
-      } else {
-        if (JSON.stringify(req.body) === '{}') {
-          res.status(400).send(`Bad request body and/or content type (${req.headers['content-type']})`)
-          return
-        }
-        try {
-          await stream.save({ body: req.body, contentType: req.headers['content-type'] })
-        } catch (err) {
-          return next(err)
-        }
-        return res.send('Ok')
+      if (JSON.stringify(req.body) === '{}') {
+        res.status(400).send(`Bad request body and/or content type (${req.headers['content-type']})`)
+        return
       }
+      try {
+        await stream.save({ body: req.body, contentType: req.headers['content-type'] })
+      } catch (err) {
+        return next(err)
+      }
+      return res.send('Ok')
     } else {
       next()
     }
